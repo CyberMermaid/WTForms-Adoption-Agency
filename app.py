d74d5008f1991cab935b09f38ca5452e5d84ecb1
CyberMermaid <45833464+CyberMermaid@users.noreply.github.com>
2024-02-17 18:05:43 -0500

    Step1 Completed

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Testing'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+debug.init_app(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
